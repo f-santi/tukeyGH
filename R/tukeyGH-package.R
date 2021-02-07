41b78479c0c22c25f98de5df9fdde54ded620fdc
Flavio Santi <flavio.santi@univr.it>
2021-02-07 16:16:21 +0100

    Package presentation page

---
+++ R/tukeyGH-package.R
@@ -0,0 +1,22 @@
+
+#' Tukey's g-and-h probability distribution
+#'
+#' It provides distribution, density and quantile functions of the Tukey's
+#' g-and-h probability distribution, as well as functions for random number
+#' generation, parameter fitting and testing.
+#'
+#' @encoding UTF-8
+#'
+#' @docType package
+#' @name tukeyGH-package
+#' @aliases tukeyGH-package
+#'
+#' @import magrittr
+#'
+#' @importFrom stats uniroot
+#'
+NULL
+
+utils::globalVariables('.')
+
+
