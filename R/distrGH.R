b9de40144ee777d5d7c7cd07dff64fa2fa96eeab
Flavio Santi <flavio.santi@univr.it>
2021-03-02 13:52:57 +0100

    Revised the help of *gh

--- R/distrGH.R
+++ R/distrGH.R
@@ -2,51 +2,21 @@
 #' The Tukey g-and-h distribution
 #' 
 #' Density, distribution function, quantile function and random generation for
-#' the Tukey g-and-h distribution \insertCite{tukey1977;textual}{tukeyGH}
+#' the Tukey g-and-h distribution \insertCite{tukey1977;textual}{tukeyGH}.
 #' 
 #' @param p vector of probabilities.
-#' @param g skewness parameter.
-#' @param h heavy-taildness parameter.
-#' @param a location parameter.
-#' @param b scale parameter.
+#' @param g skewness parameter(s).
+#' @param h heavy-taildness parameter(s).
+#' @param a location parameter(s).
+#' @param b scale parameter(s).
 #' @param ... arguments passed to `uniroot`.
+#' @param x vector of quantiles.
+#' @inheritParams stats::rnorm
 #' 
 #' @references
 #' \insertAllCited{}
 #' 
 #' @name gh
-#' 
-#' @export
-qgh <- function(p, a = 0, b = 1, g = 0, h = 1) {
-  # check the params
-  if (!is_GHvalid(a = a, b = b, g = g, h = h)) { stop('Bad parameter value') }
-  
-  # vectorisation
-  x <- data.frame(p = p, a = a, b = b, g = g, h = h)
-  rm(p, a, b, g, h)
-  
-  # normal
-  z <- qnorm(x$p, 0, 1)
-  
-  # h
-  out <- exp(x$h * z^2 / 2)
-  
-  # g
-  pos <- which(x$g == 0)
-  out[pos] <- z[pos] * out[pos]
-  pos <- which(x$g != 0)
-  out[pos] <- (exp(x$g[pos] * z[pos]) - 1) / x$g[pos] * out[pos]
-
-  # location and scale
-  out <- x$a + x$b * out
-  
-  # output
-  return(out)
-}
-
-
-
-#' @rdname gh
 #' 
 #' @export
 pgh <- function(x, a = 0, b = 1, g = 0, h = 1, ...) {
@@ -71,6 +41,38 @@ pgh <- function(x, a = 0, b = 1, g = 0, h = 1, ...) {
     }) %>%
     unlist() %>%
     return()
+}
+
+
+
+#' @rdname gh
+#' 
+#' @export
+qgh <- function(p, a = 0, b = 1, g = 0, h = 1) {
+  # check the params
+  if (!is_GHvalid(a = a, b = b, g = g, h = h)) { stop('Bad parameter value') }
+  
+  # vectorisation
+  x <- data.frame(p = p, a = a, b = b, g = g, h = h)
+  rm(p, a, b, g, h)
+  
+  # normal
+  z <- qnorm(x$p, 0, 1)
+  
+  # h
+  out <- exp(x$h * z^2 / 2)
+  
+  # g
+  pos <- which(x$g == 0)
+  out[pos] <- z[pos] * out[pos]
+  pos <- which(x$g != 0)
+  out[pos] <- (exp(x$g[pos] * z[pos]) - 1) / x$g[pos] * out[pos]
+  
+  # location and scale
+  out <- x$a + x$b * out
+  
+  # output
+  return(out)
 }
 
 
