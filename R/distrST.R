16444ec0236e62747dd0650f14a09981b8681246
Flavio Santi <flavio.santi@univr.it>
2021-03-07 15:55:04 +0100

    Added skewed-t distribution

---
+++ R/distrST.R
@@ -0,0 +1,29 @@
+
+# Density of the skewed-t distribution - Fernandez and Steel (1998)
+dst <- function (x, df = 10, a = 0, b = 1, skew = 1, log = FALSE) {
+  if (log) {
+    lnumerator <- log(2) - log(skew + 1 / skew)
+    ldenom1 <- lbeta(0.5, df / 2) + log(b) + log(df) / 2
+    ldenom2 <- (df + 1) * log(1 + (x - a)^2 / (skew^(2 * sign(x - a)) * df * b^2)) / 2
+    out <- lnumerator - ldenom1 - ldenom2
+  } else {
+    numerator <- 2 / (skew + 1 / skew)
+    denom1 <- beta(0.5, df / 2) * b * sqrt(df)
+    denom2 <- (1 + (x - a)^2 / (skew^(2 * sign(x - a)) * df * b^2))^((df + 1) / 2)
+    out <- numerator / (denom1 * denom2)
+  }
+  
+  return(out)
+}
+
+
+
+# log-likelihood of the skew-t distribution of Fernandez and Steel (1998)
+# theta = c(df, skew)
+loglikST <- function(theta, x) {
+  dst(x, df = theta[1], a = 0, b = 1, skew = theta[2], log = TRUE) %>%
+    sum() %>%
+    return()
+}
+
+
