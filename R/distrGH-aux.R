f-santi/tukeyGH

commit a89effef9ac8b881b325cf20130de62f2add73dd

Computational optimisation

R/distrGH-aux.R
@@ -28,21 +28,15 @@ gen_GHvalid <- function(n, a, b, g, h) {
 
 
 # Derivative of gh transformation
+# NOTE: all arguments must be valid and vectorised
 deriv_Tgh <- function(x, b, g, h) {
-  # check the params
-  if (!is_GHvalid(b = b, g = g, h = h)) { stop('Bad parameter value') }
-  
-  # vectorisation
-  xdf <- data.frame(x = x, b = b, g = g, h = h, p = NA)
-  rm(x, b, g, h)
-  
   # computation
-  out <- xdf$b * exp(xdf$h * xdf$x^2 / 2)
-  pos <- which(xdf$g == 0)
-  out[pos] <- (1 + xdf$h[pos] * xdf$x[pos]^2) * out[pos]
-  pos <- which(xdf$g != 0)
-  out[pos] <- with(xdf, ((g[pos] + h[pos] * x[pos]) * exp(g[pos] * x[pos]) -
-    h[pos] * x[pos]) / g[pos]) * out[pos]
+  out <- b * exp(h * x^2 / 2)
+  pos <- which(g == 0)
+  out[pos] <- (1 + h[pos] * x[pos]^2) * out[pos]
+  pos <- which(g != 0)
+  out[pos] <- (g[pos] + h[pos] * x[pos]) * exp(g[pos] * x[pos]) -
+    h[pos] * x[pos] / g[pos] * out[pos]
   
   # output
   return(out)

commit f25f6deb629b3b80fbe56a1e2d7436d262c3ba7e

Bug introduced in the previous commit (a89effe)

R/distrGH-aux.R
@@ -35,8 +35,8 @@ deriv_Tgh <- function(x, b, g, h) {
   pos <- which(g == 0)
   out[pos] <- (1 + h[pos] * x[pos]^2) * out[pos]
   pos <- which(g != 0)
-  out[pos] <- (g[pos] + h[pos] * x[pos]) * exp(g[pos] * x[pos]) -
-    h[pos] * x[pos] / g[pos] * out[pos]
+  out[pos] <- ((g[pos] + h[pos] * x[pos]) * exp(g[pos] * x[pos]) -
+    h[pos] * x[pos]) / g[pos] * out[pos]
   
   # output
   return(out)