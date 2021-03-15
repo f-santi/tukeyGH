a1a54971e78369123f93604984449be30b9885f0
Flavio Santi <flavio.santi@univr.it>
2021-03-15 18:42:44 +0100

    Added test for "gh_hoaglin1985"

---
+++ tests/testthat/test-gh.R
@@ -0,0 +1,15 @@
+
+test_that("GH fitting through Hoaglin (1985) works", {
+  # Generate data
+  theta <- gen_GHvalid(1)
+  p <- (1:99) / 100
+  z <- qnorm(p)
+  x <- lapply(z, Tgh, a = theta$a, b = theta$b, g = theta$g, h = theta$h) %>%
+    unlist()
+  
+  # Fit the distribution
+  est <- unname(gh_hoaglin1985(x)$estimate)
+  
+  # Check
+  expect_equal(est[1], theta$a)
+})
