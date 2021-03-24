aa68ab56e8c4880488fe0bfca3259bdcb98b8e12
Flavio Santi <flavio.santi@univr.it>
2021-03-24 16:34:32 +0100

    Reorganised scripts

--- R/fitGH-aux.R
+++ R/fitGH-aux.R
@@ -41,3 +41,61 @@ fitGH_mle_sub2 <- function(init, x) {
 }
 
 
+
+# Bootstrap estimation of matrix Xi
+XiBoot <- function(x, nboot, init) {
+  # initialisation
+  n <- length(x)
+  out <- matrix(0, nboot, 2)
+  
+  for (i in seq_len(nboot)) {
+    # bootstrap
+    xboot <- x[sample(n, n, replace = TRUE)]
+    # quantile estimator
+    Qest <- fitGH_hoaglin1985(x)$estimate
+    # standardisation
+    xstd <- (xboot - Qest[1]) / Qest[2]
+    # pseudo MLE
+    optim(
+      par = init,
+      fn = function(theta, x) { loglikST(c(0, 1, theta), x) },
+      x = xstd,
+      control = list(fnscale = -1)
+    ) -> depo
+    # results
+    out[i, ] <- depo$par
+  }
+  
+  # output
+  stats::cov(out)
+}
+
+
+
+# 
+iinferenceGH_ST <- function(x, parmt, W, nsim) {
+  # simulation
+  xsim <- rgh(n = nsim, a = 0, b =  1, g = x[1], h = x[2])
+  
+  # optimisation
+  hatPsi <- optim(
+    par = c(0.1, 0.5),
+    fn = function(theta, x) { loglikST(c(0, 1, theta), x) },
+    x = xsim,
+    method = "L-BFGS-B",
+    lower = c(-Inf, 0),
+    upper = c(Inf, Inf),
+    control = list(fnscale = -1)
+  )
+  
+  # estimation
+  hatPsi %>%
+    use_series('par') %>%
+    { t(parmt - .) %*% W %*% (parmt - .) } %>%
+    as.vector() %>%
+    return()
+}
+
+
+
+
