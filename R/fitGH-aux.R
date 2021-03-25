a4c3d95ab70ffbb5370e64df4797ba13c68f5614
Flavio Santi <flavio.santi@univr.it>
2021-03-25 18:17:36 +0100

    Typo on function call

--- R/fitGH-aux.R
+++ R/fitGH-aux.R
@@ -33,7 +33,7 @@ fitGH_mle_sub2 <- function(init, x) {
   optim(
     par = init,
     fn = function(theta, xdata) { loglikGH(c(0, 1, theta), xdata) },
-    x = x,
+    xdata = x,
     method = 'L-BFGS-B',
     lower = c(-Inf, 0),
     control = list(fnscale = -1)
